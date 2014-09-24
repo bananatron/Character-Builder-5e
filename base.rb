6cef2c8c30630c599586e046fcbbc3e69aa7cf95
bananatron <bananatrongo@gmail.com>
2014-09-24 17:57:55 +0000

    base for easy spell db

--- base.rb
+++ base.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+#format [name, casttime, range, components, duration, desc]
+
+@cantrips = 
+[{'name'=>'Guidance', 
+  'casttime'=>'1 action',
+  'range'=>'Touch',
+  'components'=>['v', 's'],
+  'duration'=>'Concentration, up to 1 minute',
+  'desc'=>'You touch one willing creature. Once before the spell ends, the target can roll a d4 and add the number rolled to one ability check of its choice. It can roll the die before or after making the ability check. The spell then ends.'
+  },
+    ]
+
+
 # Set port for compatability with nitrous.io
 configure :development do
   set :bind, '0.0.0.0'
