0992ed79f1f2f8765eeefa9416fb5b1bff8a4fe0
bananatron <bananatrongo@gmail.com>
2014-11-05 18:06:15 +0000

    equip cleanup

--- base.rb
+++ base.rb
@@ -59,7 +59,7 @@ post '/sheet' do
   @ch_classtool = params[:ch_classtool] #Class tool prof.
   @ch_spellcount = params[:ch_spellcount]
   @ch_skillcount = params[:ch_skillcount]
-  @ch_classequip = params[:ch_classequip]
+  @ch_classequip = params[:ch_classequip].gsub!(",",", ")
   @ch_classfeat = params[:ch_classfeat]
   
   #Alignemnt
@@ -81,7 +81,7 @@ post '/sheet' do
   @ch_bg_lang = params[:ch_bg_lang] #extra bg languages
   
   #Equip concat
-  @master_equip_list = @ch_bg_equip + @ch_classequip
+  @master_equip_list = @ch_bg_equip.strip + ", " + @ch_classequip.strip
   
   #Extra Languages
   @master_lang_count = @ch_race_langcount.to_i + @ch_bg_lang.to_i
